ce131226c310367a246e5e638de9de6e0ad09e9a
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2022-11-13 22:08:29 -0800

    final sumall tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,10 @@
 const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 ||
+        typeof num1 !== 'number' || 
+        typeof num2 !== 'number') {
+            return "ERROR"
+        }
+    
     if (num1 >= num2) { return sumSmallestToLargest(num2, num1)} ;  
     return sumSmallestToLargest(num1, num2)
 };
